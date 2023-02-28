2583e9fe850aa05e33857ad176177c94e3ea0bd2
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2023-02-28 10:57:53 +0100

    Add Lesson 4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,12 @@
+﻿using Lesson4;
+
+string testovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+                       + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                       + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                       + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                       + "posledni veta!";
+                       
+StringStatistics text = new StringStatistics(testovaciText);
+
+Console.WriteLine(text.NumberOfWords());
+Console.WriteLine(text.NumberOfSentences());
---
+++ Lesson4/StringStatistics.cs
@@ -0,0 +1,63 @@
+﻿namespace Lesson4;
+
+public class StringStatistics
+{
+    private string _text;
+    
+    public StringStatistics(string text)
+    {
+        _text = text;
+    }
+    
+    public int NumberOfWords()
+    {
+        int numberOfWords = 0;
+        bool isWord = false;
+        
+        foreach (char character in _text)
+        {
+            if (char.IsLetter(character))
+            {
+                if (isWord)
+                    continue;
+                
+                numberOfWords++;
+                isWord = true;
+            }
+            else
+            {
+                isWord = false;
+            }
+        }
+        
+        return numberOfWords;
+    }
+    
+    public int NumberOfRows() =>
+        _text.Count(c => c =='\n');
+    
+    public int NumberOfSentences()
+    {
+        int numberOfSentences = 0;
+        bool isSentence = false;
+
+        for (int i = 0; i < _text.Length; i++)
+        {
+            if (_text[i] is not '.' or '!' or '?')
+                continue;
+            
+            if (i + 2 >= _text.Length)
+            {
+                numberOfSentences++;
+                break;
+            }
+            
+            if (!char.IsLower(_text[i + 1]) || !char.IsLower(_text[i + 2]))
+            {
+                numberOfSentences++;
+            }
+        }
+        
+        return numberOfSentences;
+    }
+}
